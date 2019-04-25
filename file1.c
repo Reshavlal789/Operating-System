76e60f699867cc782089c621b6e1d466bfc5f617
Reshavlal789 <49993761+Reshavlal789@users.noreply.github.com>
2019-04-25 23:13:01 +0530

    Update file1.c

--- file1.c
+++ file1.c
@@ -20,7 +20,6 @@ P3            13                19
 P4            26                42
 Develop a scheduler which submits the processes to the processor in the defined scenario, and compute the scheduler
 performance by providing the waiting time for process, turnaround time for process and average waiting time and turnaround time.
-
 */
 
 
